75206113e0f315ceefa598740507f13cb107540e
Alexis Grojean <alexis.grojean@ledger.fr>
2023-10-13 10:51:59 +0200

    Format

--- src/infos.rs
+++ src/infos.rs
@@ -1,12 +1,12 @@
-const MAX_METADATA_STRING_SIZE : usize = 32;
+const MAX_METADATA_STRING_SIZE: usize = 32;
 
 const fn copy_str_to_u8_array(input: &str) -> [u8; MAX_METADATA_STRING_SIZE] {
     let mut result = [0u8; MAX_METADATA_STRING_SIZE];
     let bytes = input.as_bytes();
     let mut count = 0u32;
-    
+
     // Ensure we don't copy more than MAX_METADATA_STRING_SIZE
-    let min_length = core::cmp::min(bytes.len(), MAX_METADATA_STRING_SIZE - 1); 
+    let min_length = core::cmp::min(bytes.len(), MAX_METADATA_STRING_SIZE - 1);
     // Copy bytes to array
     loop {
         if count >= min_length as u32 {
@@ -33,7 +33,7 @@ static API_LEVEL: u8 = if cfg!(target_os = "nanos") {
 
 #[used]
 #[link_section = "ledger.api_level"]
-static ELF_API_LEVEL: [u8;4] = if cfg!(target_os = "nanos"){
+static ELF_API_LEVEL: [u8; 4] = if cfg!(target_os = "nanos") {
     *b"0\n\0\0"
 } else if cfg!(target_os = "nanox") {
     *b"5\n\0\0"
@@ -45,7 +45,7 @@ static ELF_API_LEVEL: [u8;4] = if cfg!(target_os = "nanos"){
 
 #[used]
 #[link_section = "ledger.target"]
-static ELF_TARGET: [u8;8] = if cfg!(target_os = "nanos"){
+static ELF_TARGET: [u8; 8] = if cfg!(target_os = "nanos") {
     *b"nanos\n\0\0"
 } else if cfg!(target_os = "nanox") {
     *b"nanox\n\0\0"
@@ -57,7 +57,7 @@ static ELF_TARGET: [u8;8] = if cfg!(target_os = "nanos"){
 
 #[used]
 #[link_section = "ledger.target_id"]
-static ELF_TARGET_ID: [u8;11] = if cfg!(target_os = "nanos"){
+static ELF_TARGET_ID: [u8; 11] = if cfg!(target_os = "nanos") {
     *b"0x31100004\n"
 } else if cfg!(target_os = "nanox") {
     *b"0x33000004\n"
@@ -69,7 +69,7 @@ static ELF_TARGET_ID: [u8;11] = if cfg!(target_os = "nanos"){
 
 #[used]
 #[link_section = "ledger.target_name"]
-static ELF_TARGET_NAME: [u8;14] = if cfg!(target_os = "nanos"){
+static ELF_TARGET_NAME: [u8; 14] = if cfg!(target_os = "nanos") {
     *b"TARGET_NANOS\n\0"
 } else if cfg!(target_os = "nanox") {
     *b"TARGET_NANOX\n\0"
@@ -81,20 +81,22 @@ static ELF_TARGET_NAME: [u8;14] = if cfg!(target_os = "nanos"){
 
 #[used]
 #[link_section = "ledger.sdk_hash"]
-static ELF_SDK_HASH: [u8;5] = *b"None\n";
+static ELF_SDK_HASH: [u8; 5] = *b"None\n";
 
 #[used]
 #[link_section = "ledger.sdk_name"]
-static ELF_SDK_NAME: [u8;18] = *b"ledger-secure-sdk\n";
+static ELF_SDK_NAME: [u8; 18] = *b"ledger-secure-sdk\n";
 
 #[used]
 #[link_section = "ledger.sdk_version"]
-static ELF_SDK_VERSION: [u8;5] = *b"None\n";
+static ELF_SDK_VERSION: [u8; 5] = *b"None\n";
 
 #[used]
 #[link_section = "ledger.rust_sdk_name"]
-static ELF_RUST_SDK_NAME: [u8;MAX_METADATA_STRING_SIZE] = copy_str_to_u8_array(env!("CARGO_PKG_NAME"));
+static ELF_RUST_SDK_NAME: [u8; MAX_METADATA_STRING_SIZE] =
+    copy_str_to_u8_array(env!("CARGO_PKG_NAME"));
 
 #[used]
 #[link_section = "ledger.rust_sdk_version"]
-static ELF_RUST_SDK_VERSION: [u8;MAX_METADATA_STRING_SIZE] = copy_str_to_u8_array(env!("CARGO_PKG_VERSION"));
+static ELF_RUST_SDK_VERSION: [u8; MAX_METADATA_STRING_SIZE] =
+    copy_str_to_u8_array(env!("CARGO_PKG_VERSION"));
