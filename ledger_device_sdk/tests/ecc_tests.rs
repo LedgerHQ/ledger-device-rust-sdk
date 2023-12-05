4a7edec30259de688de0694cfadf3654b2b79480
GroM <jerome.caporossi@gmail.com>
2023-12-05 15:03:52 +0100

    update CI with integration tests

---
+++ ledger_device_sdk/tests/ecc_tests.rs
@@ -0,0 +1,80 @@
+#![no_std]
+#![no_main]
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(ledger_device_sdk::testing::sdk_test_runner)]
+
+use core::panic::PanicInfo;
+use ledger_device_sdk::testing::{debug_print, test_panic, to_hex, TestType};
+use testmacro::test_item as test;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic(info);
+}
+
+#[no_mangle]
+extern "C" fn sample_main() {
+    test_main();
+    ledger_device_sdk::exit_app(0);
+}
+
+/// Integration tests: ECC module
+use ledger_device_sdk::ecc::{
+    make_bip32_path, BrainpoolP256R1, BrainpoolP256T1, BrainpoolP320R1, BrainpoolP320T1,
+    BrainpoolP384R1, BrainpoolP384T1, BrainpoolP512R1, BrainpoolP512T1, Ed25519, Secp256k1,
+    Secp256r1, Secp384r1, SeedDerive, Stark256,
+};
+
+const PATH: [u32; 5] = make_bip32_path(b"m/44'/123'/0'/0/0");
+
+#[test]
+fn test_secp256k1() {
+    let key: [u8; 32] = [0u8; 32];
+
+    let _priv_key = Secp256k1::new();
+    let _priv_key = Secp256k1::from(&key);
+    let priv_key = Secp256k1::derive_from_path(&PATH);
+
+    let hash = b"Not your Keys, not your Coins";
+
+    let signature = priv_key.deterministic_sign(hash).unwrap();
+
+    let pub_key = priv_key.public_key().unwrap();
+
+    assert!(pub_key.verify((&signature.0, signature.1), hash));
+}
+
+#[test]
+fn test_secp256r1() {
+    let key: [u8; 32] = [0u8; 32];
+
+    let _priv_key = Secp256r1::new();
+    let _priv_key = Secp256r1::from(&key);
+    let priv_key = Secp256r1::derive_from_path(&PATH);
+
+    let hash = b"Not your Keys, not your Coins";
+
+    let signature = priv_key.deterministic_sign(hash).unwrap();
+
+    let pub_key = priv_key.public_key().unwrap();
+
+    assert!(pub_key.verify((&signature.0, signature.1), hash));
+}
+
+#[test]
+fn test_stark256() {
+    let key: [u8; 32] = [0u8; 32];
+
+    let _priv_key = Stark256::new();
+    let _priv_key = Stark256::from(&key);
+    let priv_key = Stark256::derive_from_path(&PATH);
+
+    let hash = b"Not your Keys, not your Coins";
+
+    let signature = priv_key.deterministic_sign(hash).unwrap();
+
+    let pub_key = priv_key.public_key().unwrap();
+
+    assert!(pub_key.verify((&signature.0, signature.1), hash));
+}
