1d261c2f3a434f7b4d698b49498111974709bef7
Alexis Grojean <alexis.grojean@ledger.fr>
2024-05-22 18:32:19 +0200

    Fix CI tests, update NBGL examples.

---
+++ ledger_device_sdk/examples/nbgl_home.rs
@@ -0,0 +1,59 @@
+#![no_std]
+#![no_main]
+
+// Force boot section to be embedded in
+use ledger_device_sdk as _;
+
+use include_gif::include_gif;
+use ledger_device_sdk::io::*;
+use ledger_device_sdk::nbgl::{NbglGlyph, NbglHomeAndSettings};
+use ledger_device_sdk::nvm::*;
+use ledger_device_sdk::NVMData;
+use ledger_secure_sdk_sys::*;
+
+pub enum Instruction {
+    GetVersion,
+    GetAppName,
+}
+
+impl TryFrom<ApduHeader> for Instruction {
+    type Error = StatusWords;
+
+    fn try_from(value: ApduHeader) -> Result<Self, Self::Error> {
+        match value.ins {
+            3 => Ok(Instruction::GetVersion),
+            4 => Ok(Instruction::GetAppName),
+            _ => Err(StatusWords::NothingReceived),
+        }
+    }
+}
+
+#[no_mangle]
+extern "C" fn sample_main() {
+    unsafe {
+        nbgl_refreshReset();
+    }
+
+    const SETTINGS_SIZE: usize = 10;
+    #[link_section = ".nvm_data"]
+    static mut DATA: NVMData<AtomicStorage<[u8; SETTINGS_SIZE]>> =
+        NVMData::new(AtomicStorage::new(&[0u8; 10]));
+
+    let mut comm = Comm::new();
+
+    // Load glyph from 64x64 4bpp gif file with include_gif macro. Creates an NBGL compatible glyph.
+    const FERRIS: NbglGlyph =
+        NbglGlyph::from_include(include_gif!("examples/crab_64x64.gif", NBGL));
+
+    let settings_strings = [["Switch title", "Switch subtitle"]];
+    // Display the home screen.
+    NbglHomeAndSettings::new(&mut comm)
+        .glyph(&FERRIS)
+        .settings(unsafe { DATA.get_mut() }, &settings_strings)
+        .infos(
+            "Example App",
+            env!("CARGO_PKG_VERSION"),
+            env!("CARGO_PKG_AUTHORS"),
+        )
+        .show::<Instruction>();
+}
