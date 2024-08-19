a8083285b47a2dc03ceae05c2219b12907c06bea
ryan kurte <ryan@kurte.nz>
2024-08-20 10:12:33 +1200

    apply fmt

--- ledger_secure_sdk_sys/src/lib.rs
+++ ledger_secure_sdk_sys/src/lib.rs
@@ -4,7 +4,7 @@
 #![allow(non_snake_case)]
 
 use core::ffi::c_void;
-#[cfg(all(feature = "heap", not(target_os="nanos")))]
+#[cfg(all(feature = "heap", not(target_os = "nanos")))]
 use core::mem::MaybeUninit;
 
 pub mod buttons;
@@ -35,22 +35,22 @@ pub fn pic_rs_mut<T>(x: &mut T) -> &mut T {
     unsafe { &mut *ptr }
 }
 
-#[cfg(all(feature = "heap", not(target_os="nanos")))]
+#[cfg(all(feature = "heap", not(target_os = "nanos")))]
 use critical_section::RawRestoreState;
-#[cfg(all(feature = "heap", not(target_os="nanos")))]
+#[cfg(all(feature = "heap", not(target_os = "nanos")))]
 use embedded_alloc::Heap;
 
-#[cfg(all(feature = "heap", not(target_os="nanos")))]
+#[cfg(all(feature = "heap", not(target_os = "nanos")))]
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
 
-#[cfg(all(feature = "heap", not(target_os="nanos")))]
+#[cfg(all(feature = "heap", not(target_os = "nanos")))]
 struct CriticalSection;
-#[cfg(all(feature = "heap", not(target_os="nanos")))]
+#[cfg(all(feature = "heap", not(target_os = "nanos")))]
 critical_section::set_impl!(CriticalSection);
 
 /// Default empty implementation as we don't have concurrency.
-#[cfg(all(feature = "heap", not(target_os="nanos")))]
+#[cfg(all(feature = "heap", not(target_os = "nanos")))]
 unsafe impl critical_section::Impl for CriticalSection {
     unsafe fn acquire() -> RawRestoreState {}
     unsafe fn release(_restore_state: RawRestoreState) {}
@@ -61,7 +61,7 @@ unsafe impl critical_section::Impl for CriticalSection {
 /// The heap is stored in the stack, and has a fixed size.
 /// This method is called just before [sample_main].
 #[no_mangle]
-#[cfg(all(feature = "heap", not(target_os="nanos")))]
+#[cfg(all(feature = "heap", not(target_os = "nanos")))]
 extern "C" fn heap_init() {
     const HEAP_SIZE: usize = 1024;
     static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
@@ -69,7 +69,7 @@ extern "C" fn heap_init() {
 }
 
 #[no_mangle]
-#[cfg(any(not(feature = "heap"), target_os="nanos"))]
+#[cfg(any(not(feature = "heap"), target_os = "nanos"))]
 extern "C" fn heap_init() {}
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
