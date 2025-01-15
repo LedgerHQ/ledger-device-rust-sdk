2c9cbc0b41c16865a6e4ca371e0ff1fd5f3bd9f5
GroM <jerome.caporossi@ledger.fr>
2025-01-15 11:55:49 +0100

    Move unsafe to each unsafe calls

--- ledger_device_sdk/src/libcall/swap.rs
+++ ledger_device_sdk/src/libcall/swap.rs
@@ -69,90 +69,88 @@ impl Default for CreateTxParams {
 }
 
 pub fn get_check_address_params(arg0: u32) -> CheckAddressParams {
-    unsafe {
-        debug_print("=> get_check_address_params\n");
+    debug_print("=> get_check_address_params\n");
 
-        let mut libarg: libargs_t = libargs_t::default();
+    let mut libarg: libargs_t = libargs_t::default();
 
-        let arg = arg0 as *const u32;
+    let arg = arg0 as *const u32;
 
-        libarg.id = *arg;
-        libarg.command = *arg.add(1);
-        libarg.unused = *arg.add(2);
+    libarg.id = unsafe { *arg };
+    libarg.command = unsafe { *arg.add(1) };
+    libarg.unused = unsafe { *arg.add(2) };
 
-        libarg.__bindgen_anon_1 = *(arg.add(3) as *const libargs_s__bindgen_ty_1);
+    libarg.__bindgen_anon_1 = unsafe { *(arg.add(3) as *const libargs_s__bindgen_ty_1) };
 
-        let params: check_address_parameters_t =
-            *(libarg.__bindgen_anon_1.check_address as *const check_address_parameters_t);
+    let params: check_address_parameters_t =
+        unsafe { *(libarg.__bindgen_anon_1.check_address as *const check_address_parameters_t) };
 
-        let mut check_address_params: CheckAddressParams = Default::default();
+    let mut check_address_params: CheckAddressParams = Default::default();
 
-        debug_print("==> GET_DPATH_LENGTH\n");
-        check_address_params.dpath_len =
-            16usize.min(*(params.address_parameters as *const u8) as usize);
+    debug_print("==> GET_DPATH_LENGTH\n");
+    check_address_params.dpath_len =
+        16usize.min(unsafe { *(params.address_parameters as *const u8) as usize });
 
-        debug_print("==> GET_DPATH \n");
-        for i in 1..1 + check_address_params.dpath_len * 4 {
-            check_address_params.dpath[i - 1] = *(params.address_parameters.add(i));
-        }
-
-        debug_print("==> GET_REF_ADDRESS\n");
-        let mut address_length = 0usize;
-        let mut c = *(params.address_to_check.add(address_length));
-        while c != '\0' as i8 && address_length < 64 {
-            check_address_params.ref_address[address_length] = c as u8;
-            address_length += 1;
-            c = *(params.address_to_check.add(address_length));
-        }
-        check_address_params.ref_address_len = address_length;
-
-        check_address_params.result = &(*(libarg.__bindgen_anon_1.check_address
-            as *mut check_address_parameters_t))
-            .result as *const i32 as *mut i32;
-
-        check_address_params
+    debug_print("==> GET_DPATH \n");
+    for i in 1..1 + check_address_params.dpath_len * 4 {
+        check_address_params.dpath[i - 1] = unsafe { *(params.address_parameters.add(i)) };
     }
+
+    debug_print("==> GET_REF_ADDRESS\n");
+    let mut address_length = 0usize;
+    let mut c = unsafe { *(params.address_to_check.add(address_length)) };
+    while c != '\0' as i8 && address_length < 64 {
+        check_address_params.ref_address[address_length] = c as u8;
+        address_length += 1;
+        c = unsafe { *(params.address_to_check.add(address_length)) };
+    }
+    check_address_params.ref_address_len = address_length;
+
+    check_address_params.result = unsafe {
+        &(*(libarg.__bindgen_anon_1.check_address as *mut check_address_parameters_t)).result
+            as *const i32 as *mut i32
+    };
+
+    check_address_params
 }
 
 pub fn get_printable_amount_params(arg0: u32) -> PrintableAmountParams {
-    unsafe {
-        debug_print("=> get_printable_amount_params\n");
+    debug_print("=> get_printable_amount_params\n");
 
-        let mut libarg: libargs_t = libargs_t::default();
+    let mut libarg: libargs_t = libargs_t::default();
 
-        let arg = arg0 as *const u32;
+    let arg = arg0 as *const u32;
 
-        libarg.id = *arg;
-        libarg.command = *arg.add(1);
-        libarg.unused = *arg.add(2);
+    libarg.id = unsafe { *arg };
+    libarg.command = unsafe { *arg.add(1) };
+    libarg.unused = unsafe { *arg.add(2) };
 
-        libarg.__bindgen_anon_1 = *(arg.add(3) as *const libargs_s__bindgen_ty_1);
+    libarg.__bindgen_anon_1 = unsafe { *(arg.add(3) as *const libargs_s__bindgen_ty_1) };
 
-        let params: get_printable_amount_parameters_t =
-            *(libarg.__bindgen_anon_1.get_printable_amount
-                as *const get_printable_amount_parameters_t);
+    let params: get_printable_amount_parameters_t = unsafe {
+        *(libarg.__bindgen_anon_1.get_printable_amount as *const get_printable_amount_parameters_t)
+    };
 
-        let mut printable_amount_params: PrintableAmountParams = Default::default();
+    let mut printable_amount_params: PrintableAmountParams = Default::default();
 
-        debug_print("==> GET_IS_FEE\n");
-        printable_amount_params.is_fee = params.is_fee == true;
+    debug_print("==> GET_IS_FEE\n");
+    printable_amount_params.is_fee = params.is_fee == true;
 
-        debug_print("==> GET_AMOUNT_LENGTH\n");
-        printable_amount_params.amount_len = 16usize.min(params.amount_length as usize);
+    debug_print("==> GET_AMOUNT_LENGTH\n");
+    printable_amount_params.amount_len = 16usize.min(params.amount_length as usize);
 
-        debug_print("==> GET_AMOUNT\n");
-        for i in 0..printable_amount_params.amount_len {
-            printable_amount_params.amount[16 - printable_amount_params.amount_len + i] =
-                *(params.amount.add(i));
-        }
-
-        debug_print("==> GET_AMOUNT_STR\n");
-        printable_amount_params.amount_str = &(*(libarg.__bindgen_anon_1.get_printable_amount
-            as *mut get_printable_amount_parameters_t))
-            .printable_amount as *const i8 as *mut i8;
-
-        printable_amount_params
+    debug_print("==> GET_AMOUNT\n");
+    for i in 0..printable_amount_params.amount_len {
+        printable_amount_params.amount[16 - printable_amount_params.amount_len + i] =
+            unsafe { *(params.amount.add(i)) };
     }
+
+    debug_print("==> GET_AMOUNT_STR\n");
+    printable_amount_params.amount_str = unsafe {
+        &(*(libarg.__bindgen_anon_1.get_printable_amount as *mut get_printable_amount_parameters_t))
+            .printable_amount as *const i8 as *mut i8
+    };
+
+    printable_amount_params
 }
 
 extern "C" {
@@ -161,60 +159,63 @@ extern "C" {
 }
 
 pub fn sign_tx_params(arg0: u32) -> CreateTxParams {
+    debug_print("=> sign_tx_params\n");
+
+    let mut libarg: libargs_t = libargs_t::default();
+
+    let arg = arg0 as *const u32;
+
+    libarg.id = unsafe { *arg };
+    libarg.command = unsafe { *arg.add(1) };
+    libarg.unused = unsafe { *arg.add(2) };
+
+    libarg.__bindgen_anon_1 = unsafe { *(arg.add(3) as *const libargs_s__bindgen_ty_1) };
+
+    let params: create_transaction_parameters_t = unsafe {
+        *(libarg.__bindgen_anon_1.create_transaction as *const create_transaction_parameters_t)
+    };
+
+    let mut create_tx_params: CreateTxParams = Default::default();
+
+    debug_print("==> GET_AMOUNT\n");
+    create_tx_params.amount_len = 16usize.min(params.amount_length as usize);
+    for i in 0..create_tx_params.amount_len {
+        create_tx_params.amount[16 - create_tx_params.amount_len + i] =
+            unsafe { *(params.amount.add(i)) };
+    }
+
+    debug_print("==> GET_FEE\n");
+    create_tx_params.fee_amount_len = 16usize.min(params.fee_amount_length as usize);
+    for i in 0..create_tx_params.fee_amount_len {
+        create_tx_params.fee_amount[16 - create_tx_params.fee_amount_len + i] =
+            unsafe { *(params.fee_amount.add(i)) };
+    }
+
+    debug_print("==> GET_DESTINATION_ADDRESS\n");
+    let mut dest_address_length = 0usize;
+    let mut c = unsafe { *params.destination_address.add(dest_address_length) };
+    while c != '\0' as i8 && dest_address_length < 64 {
+        create_tx_params.dest_address[dest_address_length] = c as u8;
+        dest_address_length += 1;
+        c = unsafe { *params.destination_address.add(dest_address_length) };
+    }
+    create_tx_params.dest_address_len = dest_address_length;
+
+    create_tx_params.result = unsafe {
+        &(*(libarg.__bindgen_anon_1.create_transaction as *mut create_transaction_parameters_t))
+            .result as *const u8 as *mut u8
+    };
+
+    /* Reset BSS and complete application boot */
     unsafe {
-        debug_print("=> sign_tx_params\n");
-
-        let mut libarg: libargs_t = libargs_t::default();
-
-        let arg = arg0 as *const u32;
-
-        libarg.id = *arg;
-        libarg.command = *arg.add(1);
-        libarg.unused = *arg.add(2);
-
-        libarg.__bindgen_anon_1 = *(arg.add(3) as *const libargs_s__bindgen_ty_1);
-
-        let params: create_transaction_parameters_t =
-            *(libarg.__bindgen_anon_1.create_transaction as *const create_transaction_parameters_t);
-
-        let mut create_tx_params: CreateTxParams = Default::default();
-
-        debug_print("==> GET_AMOUNT\n");
-        create_tx_params.amount_len = 16usize.min(params.amount_length as usize);
-        for i in 0..create_tx_params.amount_len {
-            create_tx_params.amount[16 - create_tx_params.amount_len + i] = *(params.amount.add(i));
-        }
-
-        debug_print("==> GET_FEE\n");
-        create_tx_params.fee_amount_len = 16usize.min(params.fee_amount_length as usize);
-        for i in 0..create_tx_params.fee_amount_len {
-            create_tx_params.fee_amount[16 - create_tx_params.fee_amount_len + i] =
-                *(params.fee_amount.add(i));
-        }
-
-        debug_print("==> GET_DESTINATION_ADDRESS\n");
-        let mut dest_address_length = 0usize;
-        let mut c = *params.destination_address.add(dest_address_length);
-        while c != '\0' as i8 && dest_address_length < 64 {
-            create_tx_params.dest_address[dest_address_length] = c as u8;
-            dest_address_length += 1;
-            c = *params.destination_address.add(dest_address_length);
-        }
-        create_tx_params.dest_address_len = dest_address_length;
-
-        create_tx_params.result = &(*(libarg.__bindgen_anon_1.create_transaction
-            as *mut create_transaction_parameters_t))
-            .result as *const u8 as *mut u8;
-
-        /* Reset BSS and complete application boot */
         c_reset_bss();
         c_boot_std();
-
-        #[cfg(any(target_os = "stax", target_os = "flex"))]
-        NbglSpinner::new().text("Signing").show();
-
-        create_tx_params
     }
+
+    #[cfg(any(target_os = "stax", target_os = "flex"))]
+    NbglSpinner::new().text("Signing").show();
+
+    create_tx_params
 }
 
 pub enum SwapResult<'a> {
@@ -224,23 +225,21 @@ pub enum SwapResult<'a> {
 }
 
 pub fn swap_return(res: SwapResult) {
-    unsafe {
-        match res {
-            SwapResult::CheckAddressResult(&mut ref p, r) => {
-                *(p.result) = r;
-            }
-            SwapResult::PrintableAmountResult(&mut ref p, s) => {
-                if s.len() < (MAX_PRINTABLE_AMOUNT_SIZE - 1).try_into().unwrap() {
-                    for (i, c) in s.chars().enumerate() {
-                        *(p.amount_str.add(i)) = c as i8;
-                    }
-                    *(p.amount_str.add(s.len())) = '\0' as i8;
+    match res {
+        SwapResult::CheckAddressResult(&mut ref p, r) => {
+            unsafe { *(p.result) = r };
+        }
+        SwapResult::PrintableAmountResult(&mut ref p, s) => {
+            if s.len() < (MAX_PRINTABLE_AMOUNT_SIZE - 1).try_into().unwrap() {
+                for (i, c) in s.chars().enumerate() {
+                    unsafe { *(p.amount_str.add(i)) = c as i8 };
                 }
-            }
-            SwapResult::CreateTxResult(&mut ref p, r) => {
-                *(p.result) = r;
+                unsafe { *(p.amount_str.add(s.len())) = '\0' as i8 };
             }
         }
-        ledger_secure_sdk_sys::os_lib_end();
+        SwapResult::CreateTxResult(&mut ref p, r) => {
+            unsafe { *(p.result) = r };
+        }
     }
+    unsafe { ledger_secure_sdk_sys::os_lib_end() };
 }
