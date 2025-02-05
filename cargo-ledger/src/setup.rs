6136cd1e4baea11a7d172bfd320b88659a96ef08
GroM <jerome.caporossi@ledger.fr>
2025-02-05 17:00:27 +0100

    Move custom target files into cargo-ledger crate

--- cargo-ledger/src/setup.rs
+++ cargo-ledger/src/setup.rs
@@ -13,7 +13,7 @@ pub fn install_targets() {
     let sysroot_cmd = std::str::from_utf8(&sysroot_cmd).unwrap().trim();
 
     let target_files_url = Path::new(
-        "https://raw.githubusercontent.com/LedgerHQ/ledger-device-rust-sdk/a7fb841160df34b8de268b136704c8b2ed8f9973/ledger_device_sdk/"
+        "https://raw.githubusercontent.com/LedgerHQ/ledger-device-rust-sdk/y333/nbgl_support_for_nanos/cargo-ledger/"
     );
     let sysroot = Path::new(sysroot_cmd).join("lib").join("rustlib");
 
