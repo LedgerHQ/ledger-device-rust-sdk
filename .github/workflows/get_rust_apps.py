9976ea1acfc09dea532ae57cc068386c8b97aa37
GroM <jerome.caporossi@ledger.fr>
2025-03-11 09:55:22 +0100

    Try to parallelize all builds

--- .github/workflows/get_rust_apps.py
+++ .github/workflows/get_rust_apps.py
@@ -1,16 +1,23 @@
 from ledgered.github import GitHubLedgerHQ, NoManifestException
 from github.GithubException import GithubException
+
 import sys
+import json
 
 if len(sys.argv) != 2:
     print("Usage: get_rust_apps.py <github_token>")
     sys.exit(1)
 
+ledger_devices = ["nanos+", "nanox", "stax", "flex"]
+filtered_apps = ["app-kadena-legacy", "app-pocket", "app-age", "app-provenance"]
+
+# Retrieve all apps on LedgerHQ GitHub organization
 token = sys.argv[1]
 gh = GitHubLedgerHQ(token)
 apps=gh.apps
 
 rust_apps = []
+exclude_apps = []
 # loop all apps in gh.apps
 for app in apps:
     try: 
@@ -20,9 +27,22 @@ for app in apps:
     except GithubException as e:
         pass
     else:
+        # Filter out apps that are Rust based
         if manifest.app.sdk == "rust":
-            # store app_name in a list
-            rust_apps.append(app.name)
+                rust_apps.append(app.name)
+                # filter out app for specific devices
+                for d in ledger_devices:
+                    if d not in manifest.app.devices or app.name in filtered_apps:
+                        exclude_apps.append({"app-name": app.name, "device": d})
 
-# print the list of rust apps in between []
-print(rust_apps)
+# save the list of Rust apps in a json format:
+with open("rust_apps.json", "w") as f:
+    f.write(json.dumps(rust_apps))
+
+# save the list of Excluded apps in a json format:
+with open("exclude_apps.json", "w") as f:
+    f.write(json.dumps(exclude_apps))
+
+# save the list of Ledger devices in a json format:
+with open("ledger_devices.json", "w") as f:
+    f.write(json.dumps(ledger_devices))
