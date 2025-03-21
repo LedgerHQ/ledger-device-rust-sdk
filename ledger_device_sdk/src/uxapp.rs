134a3dc32c77b5d0218b0e3aaf741e1c7c359b53
GroM <jerome.caporossi@ledger.fr>
2025-03-21 09:32:36 +0100

    Delay lock + better ticker management

--- ledger_device_sdk/src/uxapp.rs
+++ ledger_device_sdk/src/uxapp.rs
@@ -11,8 +11,8 @@ pub use ledger_secure_sdk_sys::BOLOS_UX_IGNORE;
 pub use ledger_secure_sdk_sys::BOLOS_UX_OK;
 pub use ledger_secure_sdk_sys::BOLOS_UX_REDRAW;
 
-fn os_ux_rs(params: &bolos_ux_params_t) {
-    unsafe { os_ux(params as *const bolos_ux_params_t as *mut bolos_ux_params_t) };
+unsafe extern "C" {
+    pub unsafe static mut G_ux_params: bolos_ux_params_t;
 }
 
 #[repr(u8)]
@@ -21,31 +21,46 @@ pub enum UxEvent {
     Keyboard = BOLOS_UX_KEYBOARD,
     WakeUp = BOLOS_UX_WAKE_UP,
     ValidatePIN = BOLOS_UX_VALIDATE_PIN,
-    LastID = BOLOS_UX_VALIDATE_PIN + 1,
+    DelayLock = BOLOS_UX_DELAY_LOCK,
+    LastID = BOLOS_UX_DELAY_LOCK + 1,
 }
 
 impl UxEvent {
-    pub fn request(&self) -> u32 {
-        let mut params = bolos_ux_params_t::default();
-        params.ux_id = match self {
-            Self::Event => Self::Event as u8,
-            Self::Keyboard => Self::Keyboard as u8,
-            Self::WakeUp => Self::WakeUp as u8,
-            Self::ValidatePIN => {
-                // Perform pre-wake up
-                params.ux_id = Self::WakeUp as u8;
-                os_ux_rs(&params);
+    #[allow(unused)]
+    pub fn request(&self, val: Option<u32>) -> u32 {
+        unsafe {
+            //let mut params = bolos_ux_params_t::default();
+            G_ux_params.ux_id = match self {
+                Self::Event => Self::Event as u8,
+                Self::Keyboard => Self::Keyboard as u8,
+                Self::WakeUp => Self::WakeUp as u8,
+                Self::ValidatePIN => {
+                    // Perform pre-wake up
+                    G_ux_params.ux_id = Self::WakeUp as u8;
+                    os_ux(&raw mut G_ux_params as *mut bolos_ux_params_t);
 
-                Self::ValidatePIN as u8
+                    Self::ValidatePIN as u8
+                }
+                Self::DelayLock => {
+                    #[cfg(any(target_os = "stax", target_os = "flex", feature = "nano_nbgl"))]
+                    {
+                        G_ux_params.u = bolos_ux_params_s__bindgen_ty_1 {
+                            lock_delay: bolos_ux_params_s__bindgen_ty_1__bindgen_ty_3 {
+                                delay_ms: val.unwrap_or(10000),
+                            },
+                        };
+                    }
+                    Self::DelayLock as u8
+                }
+                Self::LastID => panic!("Unknown UX Event"),
+            };
+
+            os_ux(&raw mut G_ux_params as *mut bolos_ux_params_t);
+
+            match self {
+                Self::ValidatePIN => Self::block(),
+                _ => os_sched_last_status(TASK_BOLOS_UX as u32) as u32,
             }
-            Self::LastID => panic!("Unknown UX Event"),
-        };
-
-        os_ux_rs(&params);
-
-        match self {
-            Self::ValidatePIN => Self::block(),
-            _ => unsafe { os_sched_last_status(TASK_BOLOS_UX as u32) as u32 },
         }
     }
 
@@ -58,7 +73,7 @@ impl UxEvent {
                 let mut spi_buffer = [0u8; 256];
                 sys_seph::send_general_status();
                 sys_seph::seph_recv(&mut spi_buffer, 0);
-                UxEvent::Event.request();
+                UxEvent::Event.request(None);
             } else {
                 unsafe { os_sched_yield(BOLOS_UX_OK as u8) };
             }
@@ -83,7 +98,7 @@ impl UxEvent {
                 seph::seph_recv(&mut spi_buffer, 0);
                 event = comm.decode_event(&mut spi_buffer);
 
-                UxEvent::Event.request();
+                UxEvent::Event.request(None);
 
                 if let Option::Some(Event::Command(_)) = event {
                     return (ret, event);
