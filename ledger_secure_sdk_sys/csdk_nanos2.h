8b7025f4c34cccf24b328d308c320061b6da2d69
GroM <jerome.caporossi@ledger.fr>
2025-07-23 16:44:56 +0200

    Use same defines as C SDK build

--- ledger_secure_sdk_sys/csdk_nanos2.h
+++ ledger_secure_sdk_sys/csdk_nanos2.h
@@ -1,15 +1,5 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Makefile.standard_app
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
-// APP STORAGE (feature dependent)
-//#define HAVE_APP_STORAGE
-// IO SEPROXY BUFFER SIZE
-#define OS_IO_SEPH_BUFFER_SIZE 272
-// NBGL KEYBOARD (feature dependent)
-//#define NBGL_KEYBOARD
-// NBGL KEYPAD (feature dependent)
-//#define NBGL_KEYPAD
-// STANDARD DEFINES
+#define HAVE_SWAP
+#define USE_OS_IO_STACK
 #define IO_HID_EP_LENGTH 64
 #define HAVE_SPRINTF
 #define HAVE_SNPRINTF_FORMAT_U
@@ -19,15 +9,15 @@
 #define HAVE_USB_APDU
 #define USB_SEGMENT_SIZE 64
 #define HAVE_WEBUSB
-//#define WEBUSB_URL_SIZE_B 
-//#define WEBUSB_URL
-#define HAVE_IO_U2F
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Makefile.defines
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define WEBUSB_URL_SIZE_B 0
+#define WEBUSB_URL
+#define OS_IO_SEPROXYHAL
+#define STANDARD_APP_SYNC_RAPDU
+#define API_LEVEL 24
+#define REVAMPED_IO
 #define gcc
 #define __IO volatile
-
+#define NDEBUG
 #define BAGL_HEIGHT 64
 #define BAGL_WIDTH 128
 #define HAVE_BAGL_ELLIPSIS
@@ -35,20 +25,16 @@
 #define HAVE_BAGL_FONT_OPEN_SANS_EXTRABOLD_11PX
 #define HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16PX
 #define SCREEN_SIZE_NANO
-#define HAVE_UX_FLOW
-
-#define HAVE_SE_BUTTON
+#define HAVE_NBGL
+#define NBGL_STEP
+#define NBGL_USE_CASE
+#define OS_IO_SEPH_BUFFER_SIZE 272
 #define HAVE_SE_SCREEN
+#define HAVE_SE_BUTTON
 #define HAVE_FONTS
 #define HAVE_INAPP_BLE_PAIRING
 #define HAVE_BATTERY
-
 #define HAVE_LEDGER_PKI
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Misc
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//#define HAVE_LOCAL_APDU_BUFFER
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // DEBUG C SDK
