c9919392fdb99b8089e54a186f8893537aefbe21
GroM <jerome.caporossi@ledger.fr>
2025-09-18 15:28:38 +0200

    Add NbglAction use case support

---
+++ ledger_device_sdk/examples/nbgl_action.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+
+use include_gif::include_gif;
+use ledger_device_sdk::io::*;
+use ledger_device_sdk::nbgl::{
+    init_comm, NbglGlyph, NbglAction
+};
+
+ledger_device_sdk::set_panic!(ledger_device_sdk::exiting_panic);
+
+#[no_mangle]
+extern "C" fn sample_main() {
+
+    let mut comm = Comm::new();
+    init_comm(&mut comm);
+
+    #[cfg(target_os = "apex_p")]
+    const FERRIS: NbglGlyph = NbglGlyph::from_include(include_gif!("examples/crab_48x48.png", NBGL));
+    #[cfg(any(target_os = "stax", target_os = "flex"))]
+    const FERRIS: NbglGlyph = NbglGlyph::from_include(include_gif!("examples/crab_64x64.gif", NBGL));
+    #[cfg(any(target_os = "nanosplus", target_os = "nanox"))]
+    const FERRIS: NbglGlyph =
+    NbglGlyph::from_include(include_gif!("examples/crab_14x14.png", NBGL));
+
+    // Create NBGL action
+    let action = NbglAction::new()
+        .message("Press continue to proceed")
+        .action_text("Continue")
+        .glyph(&FERRIS);
+
+    action.show();
+
+    ledger_secure_sdk_sys::exit_app(0);
+
+}
