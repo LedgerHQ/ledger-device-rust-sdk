a03682d07944d5c90f28f63583a89f0ad5b218c2
GroM <jerome.caporossi@ledger.fr>
2025-09-23 17:58:40 +0200

    Update NbglSpinner

--- ledger_device_sdk/examples/nbgl_spinner.rs
+++ ledger_device_sdk/examples/nbgl_spinner.rs
@@ -1,53 +1,15 @@
 #![no_std]
 #![no_main]
 
-// Force boot section to be embedded in
-use ledger_device_sdk as _;
-
-use include_gif::include_gif;
 use ledger_device_sdk::io::*;
-use ledger_device_sdk::nbgl::{
-    init_comm, Field, NbglGlyph, NbglReview, NbglReviewStatus, NbglSpinner, StatusType,
-};
-use ledger_device_sdk::testing::debug_print;
-use ledger_secure_sdk_sys::*;
+use ledger_device_sdk::nbgl::{init_comm, NbglReviewStatus, NbglSpinner};
 
-#[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
-    exit_app(1);
-}
-
-// static spin_end: bool = false;
+ledger_device_sdk::set_panic!(ledger_device_sdk::exiting_panic);
 
 #[no_mangle]
 extern "C" fn sample_main() {
-    unsafe {
-        nbgl_refreshReset();
-    }
-
     let mut comm = Comm::new();
-    // Initialize reference to Comm instance for NBGL
-    // API calls.
     init_comm(&mut comm);
-
-    let my_field = [Field {
-        name: "Amount",
-        value: "111 CRAB",
-    }];
-
-    // Load glyph from 64x64 4bpp gif file with include_gif macro. Creates an NBGL compatible glyph.
-    const FERRIS: NbglGlyph =
-        NbglGlyph::from_include(include_gif!("examples/crab_64x64.gif", NBGL));
-    // Create NBGL review. Maximum number of fields and string buffer length can be customised
-    // with constant generic parameters of NbglReview. Default values are 32 and 1024 respectively.
-    let success = NbglReview::new()
-        .titles(
-            "Please review transaction",
-            "To send CRAB",
-            "Sign transaction\nto send CRAB",
-        )
-        .glyph(&FERRIS)
-        .show(&my_field);
 
     NbglSpinner::new().show("Please wait...");
 
@@ -60,5 +22,6 @@ extern "C" fn sample_main() {
         comm.next_event::<ApduHeader>();
         loop_count -= 1;
     }
-    NbglReviewStatus::new().show(success);
+    NbglReviewStatus::new().show(true);
+    ledger_secure_sdk_sys::exit_app(0);
 }
