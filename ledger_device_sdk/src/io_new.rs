848fe6741f44f6584b27b68e07bd6111439dd80b
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-09-25 10:40:17 +0200

    Add BOLOS APDU handling to io_new

--- ledger_device_sdk/src/io_new.rs
+++ ledger_device_sdk/src/io_new.rs
@@ -117,7 +117,12 @@ impl<const N: usize> Comm<N> {
                     offset,
                     length,
                 } => {
-                    // TODO: bolos APDUs should be precessed here, and the loop should continue
+                    // Handle BOLOS internal APDUs (CLA = 0xB0, P1 = 0x00, P2 = 0x00) internally
+                    // and continue looping until an application APDU arrives.
+                    if header.cla == 0xB0 && header.p1 == 0x00 && header.p2 == 0x00 {
+                        handle_bolos_apdu::<N>(self, header.ins);
+                        continue;
+                    }
                     return Command::new(self, header, offset, length);
                 }
                 // Explicitly convert ApduError -> StatusWords so Into<Reply> is resolved
@@ -533,4 +538,69 @@ fn reply_status_impl<const N: usize>(reply: Reply) {
         comm.pending_apdu = false;
     }
     let _ = comm.begin_tx().send(reply);
+}
+
+// BOLOS APDU Handling
+fn handle_bolos_apdu<const N: usize>(comm: &mut Comm<N>, ins: u8) {
+    match ins {
+        // Get Information INS: retrieve App name and version
+        0x01 => {
+            let mut tx = comm.begin_tx();
+            let _ = tx.append(&[0x01]);
+            const MAX_TAG_LENGTH: u8 = 32; // maximum length for the buffer containing app name/version.
+            let mut tag_buf = [0u8; MAX_TAG_LENGTH as usize];
+
+            // ---- App name ----
+
+            let name_len = unsafe {
+                os_registry_get_current_app_tag(
+                    BOLOS_TAG_APPNAME,
+                    tag_buf.as_mut_ptr(),
+                    MAX_TAG_LENGTH as u32,
+                )
+            };
+
+            if name_len > MAX_TAG_LENGTH.into() {
+                let _ = tx.send(StatusWords::Panic); // this should never happen
+                return;
+            }
+
+            let _ = tx.append(&[name_len as u8]);
+            let _ = tx.append(&tag_buf[..name_len as usize]);
+
+            // ---- App version ----
+
+            let ver_len = unsafe {
+                os_registry_get_current_app_tag(
+                    BOLOS_TAG_APPVERSION,
+                    tag_buf.as_mut_ptr(),
+                    MAX_TAG_LENGTH as u32,
+                )
+            };
+
+            if ver_len > MAX_TAG_LENGTH.into() {
+                let _ = tx.send(StatusWords::Panic); // this should never happen
+                return;
+            }
+
+            let _ = tx.append(&[ver_len as u8]);
+            let _ = tx.append(&tag_buf[..ver_len as usize]);
+
+            // ---- Flags ----
+            let flags_byte = unsafe { os_flags() } as u8;
+            // flags length (always 1 currently) then flags byte
+            let _ = tx.append(&[1]);
+            let _ = tx.append(&[flags_byte]);
+            let _ = tx.send(StatusWords::Ok);
+        }
+        // Quit Application INS
+        0xa7 => {
+            let _ = comm.begin_tx().send(StatusWords::Ok);
+            crate::exit_app(0);
+        }
+        // Unknown INS within BOLOS namespace
+        _ => {
+            let _ = comm.begin_tx().send(StatusWords::BadIns);
+        }
+    }
 }
