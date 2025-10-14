8facfabaac87ccc3644c522123b561e1af375357
GroM <jerome.caporossi@ledger.fr>
2025-10-14 14:20:17 +0200

    Improve documentation for NBGL module

--- ledger_device_sdk/src/nbgl/nbgl_advance_review.rs
+++ ledger_device_sdk/src/nbgl/nbgl_advance_review.rs
@@ -1,3 +1,14 @@
+//! A wrapper around the asynchronous NBGL [nbgl_useCaseAdvancedReview](https://github.com/LedgerHQ/ledger-secure-sdk/blob/f7ba831fc72257d282060f9944644ef43b6b8e30/lib_nbgl/src/nbgl_use_case.c#L3957) C API binding.
+//!
+//! Draws a flow of pages of a review requiring if necessary a warning page before the review.
+//! Moreover, the first and last pages of review display a top-right button, that displays more
+//! information about the warnings
+//!
+//! Navigation operates with either swipe or navigation
+//! keys at bottom right. The last page contains a long-press button with the given finishTitle and
+//! the given icon.
+//! All tag/value pairs are provided in the API and the number of pages is automatically
+//! computed, the last page being a long press one
 use super::*;
 
 struct WarningDetailsType {
@@ -7,7 +18,7 @@ struct WarningDetailsType {
     provider_message: CString,
 }
 
-/// A wrapper around the asynchronous NBGL nbgl_useCaseAdvanceReview C API binding.
+/// A builder to create and show an advanced review flow.
 pub struct NbglAdvanceReview<'a> {
     operation_type: TransactionType,
     glyph: Option<&'a NbglGlyph<'a>>,
@@ -20,6 +31,11 @@ pub struct NbglAdvanceReview<'a> {
 impl SyncNBGL for NbglAdvanceReview<'_> {}
 
 impl<'a> NbglAdvanceReview<'a> {
+    /// Creates a new advanced review flow builder.
+    /// # Arguments
+    /// * `operation_type` - The type of operation being reviewed.
+    /// # Returns
+    /// Returns a new instance of `NbglAdvanceReview`.
     pub fn new(operation_type: TransactionType) -> NbglAdvanceReview<'a> {
         NbglAdvanceReview {
             operation_type: operation_type,
@@ -31,6 +47,11 @@ impl<'a> NbglAdvanceReview<'a> {
         }
     }
 
+    /// Sets the icon to display in the center of the page.
+    /// # Arguments
+    /// * `glyph` - The icon to display in the center of the page.
+    /// # Returns
+    /// Returns the builder itself to allow method chaining.
     pub fn glyph(self, glyph: &'a NbglGlyph) -> NbglAdvanceReview<'a> {
         NbglAdvanceReview {
             glyph: Some(glyph),
@@ -38,6 +59,11 @@ impl<'a> NbglAdvanceReview<'a> {
         }
     }
 
+    /// Sets the title to display at the top of the page.
+    /// # Arguments
+    /// * `review_title` - The title to display at the top of the page.
+    /// # Returns
+    /// Returns the builder itself to allow method chaining.
     pub fn review_title(self, review_title: &str) -> NbglAdvanceReview<'a> {
         NbglAdvanceReview {
             review_title: CString::new(review_title).unwrap(),
@@ -45,6 +71,11 @@ impl<'a> NbglAdvanceReview<'a> {
         }
     }
 
+    /// Sets the subtitle to display below the title at the top of the page.
+    /// # Arguments
+    /// * `review_subtitle` - The subtitle to display below the title at the top of the page.
+    /// # Returns
+    /// Returns the builder itself to allow method chaining.
     pub fn review_subtitle(self, review_subtitle: &str) -> NbglAdvanceReview<'a> {
         NbglAdvanceReview {
             review_subtitle: CString::new(review_subtitle).unwrap(),
@@ -52,6 +83,11 @@ impl<'a> NbglAdvanceReview<'a> {
         }
     }
 
+    /// Sets the title to display on the long-press button at the bottom of the last page.
+    /// # Arguments
+    /// * `finish_title` - The title to display on the long-press button.
+    /// # Returns
+    /// Returns the builder itself to allow method chaining.
     pub fn finish_title(self, finish_title: &str) -> NbglAdvanceReview<'a> {
         NbglAdvanceReview {
             finish_title: CString::new(finish_title).unwrap(),
@@ -59,6 +95,15 @@ impl<'a> NbglAdvanceReview<'a> {
         }
     }
 
+    /// Sets the warning details to display when the user taps on the warning icon.
+    /// All parameters are optional and can be set to `None` if not needed.
+    /// # Arguments
+    /// * `dapp_provider` - The name of the dApp provider.
+    /// * `report_url` - The URL to report the issue.
+    /// * `report_provider` - The name of the report provider.
+    /// * `provider_message` - A message from the provider.
+    /// # Returns
+    /// Returns the builder itself to allow method chaining.
     pub fn warning_details(
         self,
         dapp_provider: Option<&str>,
@@ -89,6 +134,11 @@ impl<'a> NbglAdvanceReview<'a> {
         }
     }
 
+    /// Shows the advanced review flow.
+    /// # Arguments
+    /// * `fields` - A slice of `Field` representing the tag/value pairs to display.
+    /// # Returns
+    /// Returns a `SyncNbgl` instance to manage the synchronous NBGL flow.
     pub fn show(&self, fields: &[Field]) -> SyncNbgl {
         unsafe {
             let v: Vec<CField> = fields.iter().map(|f| f.into()).collect();
