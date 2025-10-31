511485847d587f6d4ca4673a703baf1689acc8af
GroM <jerome.caporossi@ledger.fr>
2025-10-31 12:27:52 +0100

    Add Dynamic Token unit tests

--- ledger_device_sdk/src/tlv/tlv_dynamic_token.rs
+++ ledger_device_sdk/src/tlv/tlv_dynamic_token.rs
@@ -199,12 +199,14 @@ pub fn parse_dynamic_token_tlv(payload: &[u8], out: &mut DynamicTokenOut) -> Res
     }
 
     // Check signature with PKI certificate
+    #[cfg(not(test))]
     let res = pki_check_signature(
         &mut hash,
         CERTIFICATE_PUBLIC_KEY_USAGE_COIN_META,
         CurvesId::Secp256k1,
         &mut extracted.signature,
     );
+    #[cfg(not(test))]
     if res.is_err() {
         return Err(TlvError::SignatureVerificationFailed);
     }
@@ -213,4 +215,84 @@ pub fn parse_dynamic_token_tlv(payload: &[u8], out: &mut DynamicTokenOut) -> Res
     *out = extracted.dynamic_token_out;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::assert_eq_err as assert_eq;
+    use crate::testing::TestType;
+    use crate::tlv::{parse_dynamic_token_tlv, DynamicTokenOut};
+    use testmacro::test_item as test;
+
+    const TLV_PAYLOAD: &[u8] = &[
+        0x01, 0x01, 0x90, 0x02, 0x01, 0x01, 0x03, 0x02, 0x01, 0xf5, 0x04, 0x06, 0x53, 0x6f, 0x6c,
+        0x61, 0x6e, 0x61, 0x05, 0x04, 0x47, 0x4f, 0x52, 0x4b, 0x06, 0x01, 0x06, 0x07, 0x33, 0x10,
+        0x01, 0x00, 0x11, 0x2c, 0x33, 0x38, 0x50, 0x67, 0x7a, 0x70, 0x4a, 0x59, 0x75, 0x32, 0x48,
+        0x6b, 0x69, 0x59, 0x76, 0x56, 0x38, 0x71, 0x65, 0x50, 0x46, 0x61, 0x6b, 0x42, 0x38, 0x74,
+        0x75, 0x6f, 0x62, 0x50, 0x64, 0x47, 0x6d, 0x32, 0x46, 0x46, 0x45, 0x6e, 0x37, 0x44, 0x70,
+        0x75, 0x6d, 0x70, 0x12, 0x00, 0x08, 0x48, 0x30, 0x46, 0x02, 0x21, 0x00, 0x91, 0xd2, 0x54,
+        0xb2, 0xf9, 0xe8, 0xaa, 0xd9, 0x27, 0x54, 0xfc, 0x62, 0x75, 0xb4, 0xd2, 0x18, 0x22, 0x7c,
+        0xe8, 0x93, 0x70, 0xcb, 0x72, 0x43, 0x48, 0xd5, 0xc4, 0x42, 0x6a, 0x9e, 0x95, 0xd6, 0x02,
+        0x21, 0x00, 0xd1, 0x18, 0x6c, 0x28, 0x81, 0x1f, 0x33, 0x48, 0x48, 0x86, 0x7e, 0xfa, 0x10,
+        0xb0, 0x61, 0x1a, 0xd2, 0xdf, 0xdf, 0x6b, 0x50, 0xfc, 0xec, 0xdd, 0x55, 0xc1, 0x1d, 0x57,
+        0x22, 0x41, 0xb4, 0x37,
+    ];
+
+    #[test]
+    fn test_parse_dynamic_token_tlv() {
+        let mut out = DynamicTokenOut::default();
+        let res = parse_dynamic_token_tlv(TLV_PAYLOAD, &mut out);
+        assert_eq!(res, Ok(()));
+    }
+
+    #[test]
+    fn test_parse_dynamic_token_tlv_missing_tag() {
+        let mut out = DynamicTokenOut::default();
+        let res = parse_dynamic_token_tlv(&TLV_PAYLOAD[3..], &mut out);
+        assert_eq!(res, Err(crate::tlv::TlvError::MissingMandatoryTag));
+    }
+
+    #[test]
+    fn test_parse_dynamic_token_tlv_unexpected_eof() {
+        let mut out = DynamicTokenOut::default();
+        let res = parse_dynamic_token_tlv(&TLV_PAYLOAD[..12], &mut out);
+        assert_eq!(res, Err(crate::tlv::TlvError::UnexpectedEof));
+    }
+
+    #[test]
+    fn test_parse_dynamic_token_tlv_invalid_der_length() {
+        let mut out = DynamicTokenOut::default();
+        let mut invalid_payload = TLV_PAYLOAD.to_vec();
+        invalid_payload[1] = 0xFF; // Invalid length for tag 0x01
+        let res = parse_dynamic_token_tlv(&invalid_payload, &mut out);
+        assert_eq!(res, Err(crate::tlv::TlvError::InvalidDerLength));
+    }
+
+    #[test]
+    fn test_parse_dynamic_token_tlv_length_overflow() {
+        let mut out = DynamicTokenOut::default();
+        let mut invalid_payload = TLV_PAYLOAD.to_vec();
+        invalid_payload[1] = 0x0A; // Invalid length for tag 0x01
+        let res = parse_dynamic_token_tlv(&invalid_payload, &mut out);
+        assert_eq!(res, Err(crate::tlv::TlvError::LengthOverflow));
+    }
+
+    #[test]
+    fn test_parse_dynamic_token_tlv_unknown_tag() {
+        let mut out = DynamicTokenOut::default();
+        let mut invalid_payload = TLV_PAYLOAD.to_vec();
+        invalid_payload[0] = 0x09; // Unknown tag
+        let res = parse_dynamic_token_tlv(&invalid_payload, &mut out);
+        assert_eq!(res, Err(crate::tlv::TlvError::UnknownTag));
+    }
+
+    #[test]
+    fn test_parse_dynamic_token_tlv_dupplicate_unique_tag() {
+        let mut out = DynamicTokenOut::default();
+        let mut invalid_payload = TLV_PAYLOAD.to_vec();
+        // Duplicate tag 0x01
+        invalid_payload.extend_from_slice(&[0x01, 0x01, 0x90]);
+        let res = parse_dynamic_token_tlv(&invalid_payload, &mut out);
+        assert_eq!(res, Err(crate::tlv::TlvError::DuplicateUniqueTag));
+    }
 }
