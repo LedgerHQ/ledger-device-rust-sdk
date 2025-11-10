5c6712e2071cc23bb2482cff77328aad2aba127e
GroM <jerome.caporossi@ledger.fr>
2025-11-10 16:37:55 +0100

    Add code snippets in doc

--- ledger_device_sdk/src/tlv.rs
+++ ledger_device_sdk/src/tlv.rs
@@ -1,11 +1,11 @@
-//! TLV parsers
+//! Ledger TLV parsers
 //!
 //! This module provides parsers for various TLV (Tag-Length-Value) encoded data structures.
 //! Each parser is implemented in its own submodule.
 //! The available parsers are:
-//! - Trusted Name TLV Parser [`tlv_trusted_name`](tlv_trusted_name/index.html)
-//! - Dynamic Token TLV Parser [`tlv_dynamic_token`](tlv_dynamic_token/index.html)
-//! - Generic TLV Parser [`tlv_generic`](tlv_generic/index.html)
+//! - [`tlv_trusted_name`](tlv_trusted_name/index.html)
+//! - [`tlv_dynamic_token`](tlv_dynamic_token/index.html)
+//! - [`tlv_generic`](tlv_generic/index.html)
 
 pub mod tlv_trusted_name;
 #[doc(inline)]
